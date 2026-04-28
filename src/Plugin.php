b64a585aadaa10594cd0e775f822d9da9f727748
Mehul Gohil <mehul.gohil0810@gmail.com>
2026-04-28 10:02:39 +0530

    Bootstrap REST MCP and abilities APIs

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Quark;
 
+use Quark\Rest\McpController;
+use Quark\Rest\ContentController;
+
 final class Plugin
 {
     private static ?self $instance = null;
@@ -17,8 +20,24 @@ final class Plugin
         return self::$instance;
     }
 
+    public static function activate(): void
+    {
+        flush_rewrite_rules();
+    }
+
+    public static function deactivate(): void
+    {
+        flush_rewrite_rules();
+    }
+
     public function boot(): void
     {
-        // Bootstrap core components.
+        add_action('rest_api_init', [$this, 'register_routes']);
+    }
+
+    public function register_routes(): void
+    {
+        (new McpController())->register_routes();
+        (new ContentController())->register_routes();
     }
 }

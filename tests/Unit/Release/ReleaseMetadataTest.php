01c8c3465cb3df9f608c408850a221dce5173bb5
Mehul Gohil <mehul.gohil0810@gmail.com>
2026-07-16 22:43:51 +0530

    Prepare 0.7.1 release metadata baseline (tests/Unit/Release/ReleaseMetadataTest.php)

--- tests/Unit/Release/ReleaseMetadataTest.php
+++ tests/Unit/Release/ReleaseMetadataTest.php
@@ -24,15 +24,15 @@ final class ReleaseMetadataTest extends TestCase {
 		$lockfile = $this->json_file( $root . '/package-lock.json' );
 		$log      = $this->json_file( $root . '/changelog.json' );
 
-		self::assertSame( '0.7.0', $this->header( $plugin, 'Version' ) );
-		self::assertStringContainsString( "define( 'ACULECT_AI_COMPANION_VERSION', '0.7.0' );", $plugin );
-		self::assertSame( '0.7.0', $this->header( $readme, 'Stable tag' ) );
-		self::assertSame( '0.7.0', (string) ( $package['version'] ?? '' ) );
-		self::assertSame( '0.7.0', (string) ( $lockfile['version'] ?? '' ) );
-		self::assertSame( '0.7.0', (string) ( $lockfile['packages']['']['version'] ?? '' ) );
+		self::assertSame( '0.7.1', $this->header( $plugin, 'Version' ) );
+		self::assertStringContainsString( "define( 'ACULECT_AI_COMPANION_VERSION', '0.7.1' );", $plugin );
+		self::assertSame( '0.7.1', $this->header( $readme, 'Stable tag' ) );
+		self::assertSame( '0.7.1', (string) ( $package['version'] ?? '' ) );
+		self::assertSame( '0.7.1', (string) ( $lockfile['version'] ?? '' ) );
+		self::assertSame( '0.7.1', (string) ( $lockfile['packages']['']['version'] ?? '' ) );
 
 		$release_version = preg_replace( '/-(?:alpha|beta|rc)\.\d+$/', '', (string) ( $package['version'] ?? '' ) );
-		self::assertSame( '0.7.0', $release_version );
+		self::assertSame( '0.7.1', $release_version );
 		self::assertArrayHasKey( $release_version, $log );
 		foreach ( $log as $version => $entry ) {
 			self::assertIsString( $version );
